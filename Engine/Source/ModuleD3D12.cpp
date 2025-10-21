f03419c62b3a6ddc56e176b8e50225ab9f5c0518
AndreuMiroSabate <andreu.miro@estudiantat.upc.edu>
2025-10-21 21:19:53 +0200

    create command list and queue

--- Engine/Source/ModuleD3D12.cpp
+++ Engine/Source/ModuleD3D12.cpp
@@ -13,8 +13,20 @@ bool ModuleD3D12::init()
 {
 	enbleDebugLayer();
 	createDevice();
+	createCommandList();
+	createCommandQueue();
 
 	return true;
+}
+
+void ModuleD3D12::preRender()
+{
+	
+}
+
+void ModuleD3D12::render()
+{
+	
 }
 
 bool ModuleD3D12::enbleDebugLayer()
@@ -39,16 +51,54 @@ bool ModuleD3D12::createDevice()
 	CreateDXGIFactory2(0, IID_PPV_ARGS(&factory));
 #endif
 
-	ComPtr<IDXGIAdapter4> adpater;
-	ComPtr<ID3D12Device> device;
+	
 	factory->EnumAdapterByGpuPreference(0, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&adpater));
 	D3D12CreateDevice(adpater.Get(), D3D_FEATURE_LEVEL_12_0, IID_PPV_ARGS(&device));
 
-	ComPtr<ID3D12InfoQueue> infoQueue;
+	
 	device.As(&infoQueue);
 	infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_CORRUPTION, TRUE);
 	infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, TRUE);
 	infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_WARNING, TRUE);
 
 	return true;
+}
+
+bool ModuleD3D12::createCommandQueue()
+{
+	D3D12_COMMAND_QUEUE_DESC desc = {};
+	desc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+	desc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL;
+	desc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+	desc.NodeMask = 0;
+
+	device->CreateCommandQueue(&desc, IID_PPV_ARGS(&comandQueue));
+}
+
+bool ModuleD3D12::createCommandList()
+{
+	device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&commandAllocator));
+	device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, commandAllocator.Get(), nullptr, IID_PPV_ARGS(&comandList));
+}
+bool ModuleD3D12::createSwapChain()
+{
+	DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
+
+	//Need to be changed
+	swapChainDesc.Width = 0;
+	swapChainDesc.Height = 0;
+	swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+
+	swapChainDesc.Stereo = FALSE;
+	swapChainDesc.SampleDesc = { 1,0 };
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.BufferCount = 2;
+
+	swapChainDesc.Scaling = DXGI_SCALING_STRETCH;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+
+	swapChainDesc.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
+	swapChainDesc.Flags = 0;
+
+	
 }
--- Engine/Source/ModuleD3D12.h
+++ Engine/Source/ModuleD3D12.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "Module.h"
 #include "dxgi1_6.h"
+#include "d3d12.h"
+#include <cstdint>
+
 
 
 class ModuleD3D12 :
@@ -11,12 +14,24 @@ public:
 	~ModuleD3D12();
 
 	bool init() override;
+	void preRender() override;
+	void render() override;
 
 	bool createDevice();
 	bool enbleDebugLayer();
+	bool createCommandQueue();
+	bool createCommandList();
+	bool createSwapChain();
 
 private:
 
 	ComPtr<IDXGIFactory6> factory;
+	ComPtr<IDXGIAdapter4> adpater;
+	ComPtr<ID3D12Device> device;
+	ComPtr<ID3D12InfoQueue> infoQueue;
+	ComPtr<ID3D12CommandQueue> comandQueue;
+	ComPtr<ID3D12CommandList> comandList;
+	ComPtr<ID3D12CommandAllocator> commandAllocator;
+	
 };
 
