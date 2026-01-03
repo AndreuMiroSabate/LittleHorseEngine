89a5f1d9a9c777128c5b3ed32e68bbc2d434e707
AndreuMiroSabate <andreu.miro@estudiantat.upc.edu>
2026-01-03 17:20:47 +0100

    loading vertex atttribute function

--- Engine/Source/Mesh.cpp
+++ Engine/Source/Mesh.cpp
@@ -1,2 +1,19 @@
 #include "Globals.h"
 #include "Mesh.h"
+
+
+#include "tiny_gltf.h"
+
+void Mesh::loadMesh(const tinygltf::Model& model, const tinygltf::Mesh& mesh, const tinygltf::Primitive& primitive)
+{
+	const auto& itPos = primitive.attributes.find("POSITION");
+
+	if (itPos != primitive.attributes.end())
+	{
+		uint32_t numVertices = uint32_t(model.accessors[itPos->second].count);
+		Vertex* vertices = new Vertex[numVertices];
+		uint8_t* vertexData = (uint8_t*)vertices;
+		loadAccessorData(vertexData + offsetof(Vertex, position), sizeof(Vector3), sizeof(Vertex), numVertices, model, itPos->second);
+		loadAccessorDataX(vertexData + offsetof(Vertex, texCoord0), sizeof(Vector2), sizeof(Vertex), numVertices, model, primitive.attributes, "TEXCOORD_0");
+	}
+}
--- Engine/Source/Mesh.h
+++ Engine/Source/Mesh.h
@@ -1,9 +1,55 @@
 #pragma once
+
+namespace tinygltf
+{
+	class Model;
+	struct Mesh;
+	struct Primitive;
+}
+
 class Mesh
 {
+public:
+	struct Vertex
+	{
+		Vector3 position;
+		Vector2 texCoord0;
+	};
+
+	void loadMesh(const tinygltf::Model& model, const tinygltf::Mesh& mesh, const tinygltf::Primitive& primitive);
+
+	bool loadAccessorData(uint8_t* data, size_t elemSize, size_t stride, size_t elemCount, const tinygltf::Model& model, int accesorIndex)
+	{
+		const tinygltf::Accessor& accessor = model.accessors[accesorIndex];
+		if (elemCount == accessor.count)
+		{
+			const tinygltf::BufferView& bufferView = model.bufferViews[accessor.bufferView];
+			const tinygltf::Buffer& buffer = model.buffers[bufferView.buffer];
+			size_t bufferOffset = bufferView.byteOffset + accessor.byteOffset;
+			const uint8_t* srcData = buffer.data.data() + bufferOffset;
+			for (size_t i = 0; i < elemCount; ++i)
+			{
+				memcpy(data + i * stride, srcData + i * accessor.ByteStride(bufferView), elemSize);
+			}
+			return true;
+		}
+		return false;
+	}
+
+	bool loadAccessorDataX(uint8_t* data, size_t elemSize, size_t stride, size_t elemCount, const tinygltf::Model& model, const std::map<std::string, int>& attributes, const char* accesorName)
+	{
+		const auto& it = attributes.find(accesorName);
+		if (it != attributes.end())
+		{
+			return loadAccessorData(data, elemSize, stride, elemCount, model, it->second);
+		}
+		return false;
+	}
 private:
 	std::string name;
-	//std::vector
+	
+	ComPtr<ID3D12Resource> vertexBuffer;
+	ComPtr<ID3D12Resource> indexBuffer;
 
 };
 
