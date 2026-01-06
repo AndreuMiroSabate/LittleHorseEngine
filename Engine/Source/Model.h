c4571c404ebc1c3ebf541555100c04e30f9d36c1
AndreuMiroSabate <andreu.miro@estudiantat.upc.edu>
2026-01-06 10:41:01 +0100

    load model function

--- Engine/Source/Model.h
+++ Engine/Source/Model.h
@@ -1,13 +1,15 @@
 #pragma once
+#include "Mesh.h"
+#include "BasicMaterial.h"
 class Model
 {
 
 public:
 
-	//std::vector<Mesh> meshes;
+	std::vector<Mesh*> meshes;
 	//std::vector<Nodes> nodes;
 	//std::vector<Animation> animations;
-	//std::vector<Material> materials;
+	std::vector<BasicMaterial*> materials;
 
 	void LoadModel(const char* fileName);
 };
