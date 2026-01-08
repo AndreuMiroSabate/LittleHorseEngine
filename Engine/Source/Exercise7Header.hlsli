e49f0b51cad2f0471837c9f825519026ad87ba82
AndreuMiroSabate <andreu.miro@estudiantat.upc.edu>
2026-01-08 10:44:04 +0100

    Exercice 7 PS and VS to support PBRPhong material

---
+++ Engine/Source/Exercise7Header.hlsli
@@ -0,0 +1,21 @@
+static const float PI = 3.14159265;
+
+cbuffer PerFrame : register(b1)
+{
+    float3 lightDir;
+    float3 lightColor;
+    float3 ambientColor;
+    float3 cameraPos;
+}
+
+cbuffer PerInstance : register(b2)
+{
+    float4x4 modelMat;
+    float4x4 normalMat;
+    
+    float4 diffuseColour;
+    float kDifuse;
+    float3 kSpecular;
+    float kShininess;
+    bool hasDifusseTex;
+}
---
+++ Engine/Source/Exercise7PS.hlsl
@@ -0,0 +1,21 @@
+#include "Exercise7Header.hlsli"
+
+Texture2D diffuseTex : register(t0);
+SamplerState diffuseSamp : register(s0);
+
+float4 main(float3 worldPos : POSITION, float3 normal : NORMAL, float2 coord : TEXCOORD) : SV_TARGET
+{
+    float3 colorDiffuse = hasDifusseTex ? diffuseTex.Sample(diffuseSamp, coord).rgb * diffuseColour.rgb : diffuseColour.rgb;
+    float3 N = normalize(normal);
+    float3 L = normalize(-lightDir);
+    float3 V = normalize(cameraPos - worldPos);
+    float3 R = reflect(-L, N);
+    float3 satVR = saturate(dot(R, V));
+    float satNL = saturate(dot(N, L));
+    float maxRf = max(max(kSpecular.r, kSpecular.g), kSpecular.b);
+    float3 fresnel = kSpecular + (1.0 - kSpecular) * pow(1.0 - satNL, 5.0);
+	
+    float3 colour = ((colorDiffuse*(1.0-maxRf))/PI+((kShininess + 2.0)/2*PI) * fresnel * pow(satVR, kShininess)) * lightColor * (N * L);
+	
+    return float4(colour, 1.0);
+}
